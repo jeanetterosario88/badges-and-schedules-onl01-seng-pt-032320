203184a2d002f64cb1cd16a999d1b6556bef4d3d
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-03-31 03:52:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,11 +20,9 @@ end
 
 def printer (attendees)
  batch_badge_creator(attendees).each do |attendee|
-  end
-  attendees.each_with_index do |name, index|
-    attendeeswithroomnumbers<<"Hello #{name}! You'll be assigned to room #{index+1}!"
-  end
-  attendeeswithroomnumbers.each do |attendee|
+ puts attendee
+ end
+ assign_rooms(attendees).each do |attendee|
   puts attendee
   end
 end
