906f9ed1120760b07bee2046af8d4d0ffa4aca03
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-03-31 03:04:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges=[]
+  attendees.each do |attendee|
+    badges<<"Hello, my name is #{attendee}."
+  end
+  badges
+end
+
+def assign_rooms(attendees)
+attendeeswithroomnumbers=[]
+  attendees.each_with_index do |name, index|
+  attendeeswithroomnumbers<<"Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  attendeeswithroomnumbers
+end
+
+def printer (attendees)
+ badges=[]
+  attendees.each do |attendee|
+    badges<<"Hello, my name is #{attendee}."
+  end
+  puts badges
+  attendeeswithroomnumbers=[]
+  attendees.each_with_index do |name, index|
+    attendeeswithroomnumbers<<"Hello #{name}! You'll be assigned to room #{index+1}!"
+  end
+  puts attendeeswithroomnumbers
+end
